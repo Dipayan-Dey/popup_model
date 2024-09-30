d364664ecc9ace6f734aa3facab50b17b872931d
Mr Dipayan Dey <161328487+deydipayan2006@users.noreply.github.com>
2024-09-30 11:25:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 const showOverLay = document.querySelector(".overlay");
 const showLoginForm = document.querySelector(".login-form");
-var btnLogin=document.querySelector(".primary-btn");
-var removeForm=document.querySelector(".overlay");
-const popup = document.querySelector(".secondary-btn")
+var btnLogin = document.querySelector(".primary-btn");
+var removeForm = document.querySelector(".overlay");
+const popup = document.querySelector(".secondary-btn");
+const inputs = document.querySelector(".input");
+const pass = document.getElementById("password");
 //show model
-var show = ()=> {
-      showOverLay.classList.add("show-overlay");
-      showLoginForm.classList.add("show-loginform");
-}
-btnLogin.addEventListener("click",show);
+var show = () => {
+  showOverLay.classList.add("show-overlay");
+  showLoginForm.classList.add("show-loginform");
+};
+btnLogin.addEventListener("click", show);
 //close model
- var remove = ()=>{
-    showOverLay.classList.remove("show-overlay");
-      showLoginForm.classList.remove("show-loginform");
-}
-removeForm.addEventListener("click",remove);
- // show confrime notification
-  
-var notification =()=>{
-      showOverLay.classList.remove("show-overlay");
-      showLoginForm.classList.remove("show-loginform");
-    alert("Login Successfull. Your response has been recorded")
-}
+var remove = () => {
+  showOverLay.classList.remove("show-overlay");
+  showLoginForm.classList.remove("show-loginform");
+};
+removeForm.addEventListener("click", remove);
+// show confrime notification
 
-popup.addEventListener("click",notification);
+var notification = () => {
+  if (inputs.value == "" || pass.value == "") {
+    alert("All inputs are required. please filled the input box then click the login button");
+  } else {
+    alert("Login Successfull. Your response has been recorded");
+    inputs.value = "";
+    pass.value = "";
+  }
+};
+
+popup.addEventListener("click", notification);
